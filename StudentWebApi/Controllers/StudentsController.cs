1470087e84ce64b3de9920f83b9d44e2afece934
sokkary <sokkary.aziz@gmail.com>
2019-08-08 15:56:23 +0300

    initial commit

---
+++ StudentWebApi/Controllers/StudentsController.cs
@@ -0,0 +1,77 @@
+﻿using StudentWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentWebApi.Controllers
+{
+    [RoutePrefix("api/students")]
+    public class StudentsController : ApiController
+    {
+
+        static List<Student> students = new List<Student>()
+        {
+            new Student() { Id = 1, Name = "Tom" },
+            new Student() { Id = 2, Name = "Sam" },
+            new Student() { Id = 3, Name = "John" }
+        };
+
+
+        [Route("~/api/teachers")]
+        public IEnumerable<Teacher> GetTeachers()
+        {
+            List<Teacher> teachers = new List<Teacher>()
+    {
+        new Teacher() { Id = 1, Name = "Rob" },
+                new Teacher() { Id = 2, Name = "Mike" },
+        new Teacher() { Id = 3, Name = "Mary" }
+    };
+
+            return teachers;
+        }
+
+        
+
+        //[Route("")]
+        //convention based routing
+        public IEnumerable<Student> Get()
+        {
+            return students;
+        }
+
+        [Route("{id:int}")]
+        public Student Get(int id)
+        {
+            return students.FirstOrDefault(s => s.Id == id);
+        }
+
+        [Route("{name:alpha}")]
+        public Student Get(string name)
+        {
+            return students.FirstOrDefault(s => s.Name.ToLower() == name.ToLower());
+        }
+
+        public HttpResponseMessage Post(Student student)
+        {
+            students.Add(student);
+            var response = Request.CreateResponse(HttpStatusCode.Created);
+            return response;
+        }
+
+
+        //attribute routing
+        [Route("{id}/courses")]
+        public IEnumerable<string> GetStudentCourses(int id)
+        {
+            if (id == 1)
+                return new List<string>() { "C#", "ASP.NET", "SQL Server" };
+            else if (id == 2)
+                return new List<string>() { "ASP.NET Web API", "C#", "SQL Server" };
+            else
+                return new List<string>() { "Bootstrap", "jQuery", "AngularJs" };
+        }
+    }
+}
